f4e749977603f2dbc8d66d4ceab0ac6b665ae29a
Robert Nowakowski <rnowakowski.prv@gmail.com>
2018-05-01 15:38:36 +0200

    Changing players and displaying who's turn now

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
 
@@ -29,12 +30,15 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: '+ (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
